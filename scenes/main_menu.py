e0f52751065edda0d1093318d302f2d86513cd1f
j0pl0p <matveysokolov514@gmail.com>
2022-12-23 23:37:43 +0300

    Обновлены кнопки и главное меню

--- scenes/main_menu.py
+++ scenes/main_menu.py
@@ -11,10 +11,16 @@ class MenuWindow(BaseWindow):
         self.surface = pygame.Surface((1000, 750))
         self.surface.set_alpha(0)
         self.surface.fill((0, 0, 0))
+
         background = ImageObject(game, 'data/images/mm_background.png', 0, 0)
         logo = ImageObject(game, 'data/images/logo.png', 183, 15)
+        btn_new_game = ButtonObject(game, 320, 250, 150, 50, 'black', self.start_game, text='Новая игра')
+        btn_settings = ButtonObject(game, 320, 350, 150, 50, 'black', self.open_settings, text='Настройки')
+
         self.objects.append(background)
         self.objects.append(logo)
+        self.objects.append(btn_new_game)
+        self.objects.append(btn_settings)
 
     def process_draw(self):
         super().process_draw()
