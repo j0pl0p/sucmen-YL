57a0ae6e5f922092e4dbb9f9b76bb4aa1da3ceb8
oleg-top <akpartizan@yandex.ru>
2023-01-01 21:36:58 +0300

    Сделал выбор музыки картинкой

--- buttons/switch_button.py
+++ buttons/switch_button.py
@@ -4,4 +4,40 @@ from buttons.standard_button import StandardButton
 
 
 class SwitchButton(StandardButton):
-    pass
+    def __init__(self, x, y, image, image_pressed, scale, function=None):
+        width = image.get_width()
+        height = image.get_height()
+        super().__init__(x, y, width, height, image, scale, function)
+        self.image_pressed = pygame.transform.scale(image_pressed, (int(width * scale), int(height * scale)))
+        self.other = None
+
+    def off(self):
+        self.switch = False
+
+    def connect(self, button):
+        self.other = button
+        button.other = self
+
+    def click(self):
+        super().click()
+        self.switch = True
+        self.other.off()
+
+    def is_clicked(self):
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.click()
+
+        super().is_clicked()
+
+    def draw(self, screen):
+        self.is_clicked()
+        img = {
+            True: self.image_pressed,
+            False: self.image
+        }
+        self.is_clicked()
+        screen.blit(img[self.switch], (self.rect.x, self.rect.y))
--- scenes/music_select.py
+++ scenes/music_select.py
@@ -1,6 +1,7 @@
 import pygame
 
 from buttons.back_button import BackButton
+from buttons.buttons_collection import ButtonsCollection
 from objects.button import ButtonObject
 from objects.text import TextObject
 from scenes.base import BaseWindow
@@ -10,28 +11,19 @@ class MorgenWindow(BaseWindow):
     """ Окно выбора музыки """
     def __init__(self, game):
         super().__init__(game)
-        text = {
-            'ice': 'Эй уу айс цепь на мне это айс',
-            'cadillac': 'Как дела как дела это новый кадилак'
-        }
+        self.screen = game.screen
+        self.buttons = ButtonsCollection(game)
 
-        text_chosen = TextObject(game, 500, 500, f'Выбрано:     {text["ice"]}', 'white', 'Comic Sans MS', 33)
-        btn_morgen_ice = ButtonObject(game, 320, 150, 150, 50, 'black', lambda: self.function_apply(text['ice']), text=text['ice'])
-        btn_morgen_cadillac = ButtonObject(game, 320, 200, 150, 50, 'black', lambda: self.function_apply(text['cadillac']), text=text['cadillac'])
-
-        self.back_button = BackButton(5, 20, self.game, self.function_back, text='Назад')
-        self.objects.append(btn_morgen_cadillac)
-        self.objects.append(btn_morgen_ice)
-        self.objects.append(text_chosen)
-
-    def function_back(self):
-        self.game.set_window(self.game.WINDOW_SETTINGS)
-
-    def function_apply(self, title):
-        # выбор функции
-        self.objects[2] = TextObject(self.game, 350, 500, f'Выбрано:      {title}', 'white', 'Comic Sans MS', 35)
-        self.game.set_window(self.game.WINDOW_SETTINGS)
+    def process_event(self, event):
+        super().process_event(event)
+        if event.type != pygame.KEYDOWN:
+            return
+        if event.type == pygame.K_ESCAPE:
+            self.buttons.function_back()
+            self.game.sound.mus_switch()
 
     def process_draw(self):
+        self.buttons.prev_settings()
         super().process_draw()
-        self.back_button.draw(self.game.screen)
+        for btn in self.buttons.buttons:
+            btn.draw(self.screen)
